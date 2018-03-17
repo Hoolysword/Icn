5f9f61486f2bcf3b5e93497d3f51513f6d6306b9
Olimin9 <35722730+Olimin9@users.noreply.github.com>
2018-03-17 16:31:50 +0100

    Add files via upload

---
+++ ICN Projet/mur.js
@@ -0,0 +1,42 @@
+
+function debut(){
+var bouton1=document.getElementById("appart1");
+ bouton1.addEventListener("click",appartement1);
+ var bouton2=document.getElementById("appart2");
+ bouton2.addEventListener("click",appartement2);
+ var bouton3=document.getElementById("appart3");
+ bouton3.addEventListener("click",appartement3);
+ var bouton4=document.getElementById("appart4");
+ bouton4.addEventListener("click",appartement4);
+  var bouton5=document.getElementById("appart5");
+ bouton5.addEventListener("click",appartement5); 
+ var bouton6=document.getElementById("appart6");
+ bouton6.addEventListener("click",appartement6);
+}
+
+
+function appartement1(){
+	window.location="appart1.html";
+}
+
+function appartement2(){
+	window.location.href="appart2.html";
+}
+
+function appartement3(){
+	window.location.href="appart3.html";
+}
+
+function appartement4(){
+	window.location.href="appart4.html";
+}
+
+function appartement5(){
+	window.location.href="appart5.html";
+}
+
+function appartement6(){
+	window.location.href="appart6.html";
+}
+
+window.addEventListener("load",debut)
