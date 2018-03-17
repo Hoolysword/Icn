e8890cd1912dee6cd9fc2d28d9194cfbeedc10e8
RigNat <35722579+RigNat@users.noreply.github.com>
2018-03-17 15:52:40 +0100

    Update marche2.js

--- ICN Projet/marche2.js
+++ ICN Projet/marche2.js
@@ -1,5 +1,4 @@
-var contenue=document.getElementById("contenue");
-
+var contenue;
 function debut(){
 	var bouton0=document.getElementById('bouton0');
 	bouton0.addEventListener("click",random);
@@ -25,52 +24,52 @@ function debut(){
 	boutonvalide.addEventListener("click",random10);
 }
 function random(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="0";
 }
 function random1(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="1";
 }
 function random2(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="2";
 }
 function random3(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="3";
 }
 function random4(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="4";
 }
 function random5(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="5";
 }
 function random6(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="6";
 }
 function random7(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="7";
 }
 function random8(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="8";
 }
 function random9(){
-var contenue=document.getElementById("contenue")
+contenue=document.getElementById("contenue")
 contenue.innerHTML+="9";
 }
-
-
-
-
 function random10(){
-
-document.location.href="mur.html"; }
-
-
+if (contenue.innerHTML==548){
+	document.location.href="mur.html";
+}
+else{
+	contenue=document.getElementById("contenue")
+	contenue.innerHTML=" ";
+}
+}
 window.addEventListener("load",debut);
